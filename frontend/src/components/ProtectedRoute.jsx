7bec1f7d7dbea4927b79933b201732947fbdfec4
Anas <169180642+AnasSaeed09@users.noreply.github.com>
2025-07-29 08:59:59 +0500

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,14 +1,19 @@
-import React, { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 
 export const ProtectedRoute = () => {
-  const { token } = useContext(ShopContext);
+  const { token, tokenLoading } = useContext(ShopContext);
   const location = useLocation();
   const [redirect, setRedirect] = useState(false);
 
-  if (redirect)
+  if (tokenLoading) {
+    return null; // or <FullScreenLoader />
+  }
+
+  if (redirect) {
     return <Navigate to="/login" state={{ from: location.pathname }} replace />;
+  }
 
   if (!token) {
     return (
@@ -18,7 +23,7 @@ export const ProtectedRoute = () => {
             Login Required
           </h2>
           <p className="mb-4 text-yellow-100">
-            You must login in to access this page.
+            You must login to access this page.
           </p>
           <button
             className="bg-blue-800 text-yellow-100 px-4 py-2 rounded border-2 hover:bg-blue-400 hover:shadow-md hover:shadow-blue-300 active:bg-slate-700 transition cursor-pointer"
@@ -30,5 +35,6 @@ export const ProtectedRoute = () => {
       </div>
     );
   }
+
   return <Outlet />;
 };
