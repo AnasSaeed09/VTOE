115a5c8ef467258bf6aca0683ab85a3271bd4014
Anas <169180642+AnasSaeed09@users.noreply.github.com>
2025-10-13 20:17:16 +0500

    Update MaleModel.jsx

--- frontend/src/components/model_components/MaleModel.jsx
+++ frontend/src/components/model_components/MaleModel.jsx
@@ -18,7 +18,7 @@ export function MaleModel({ adjustCloth, height, waist, shirtColor, product, set
   const humanModel = useGLTF(
   product?.category === "Men"
     ? "https://AnasSaeed09.github.io/EcoModels/tryOn-Models/Male.glb"
-    : "https://AnasSaeed09.github.io/EcoModels/tryOn-Models/Woman.glb"
+    : "https://corsproxy.io/?https://raw.githubusercontent.com/AnasSaeed09/EcoModels/main/tryOn-Models/Woman.glb"
 );
 
 
